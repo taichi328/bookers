e6ee5a5ebe477462b2503e6cba5314c33a09f9f1
EC2 Default User <125885895+taichi328@users.noreply.github.com>
2023-03-22 15:21:37 +0000

    [add]3-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  resources:homes
+  
   root to: 'homes#top'
-  get 'books/new'
-  get 'books' =>'books#index'
-  post 'books' => 'books#create'
-  get 'books/show'
-  get 'books/edit'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book' 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources:books
+  
 end
